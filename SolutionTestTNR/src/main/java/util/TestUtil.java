f2452952811ade442ddf5b2d43155fd035fc39f3
wajihwerghi <62029219+wajihwerghi@users.noreply.github.com>
2020-04-03 20:01:14 +0200

    timeout

--- SolutionTestTNR/src/main/java/util/TestUtil.java
+++ SolutionTestTNR/src/main/java/util/TestUtil.java
@@ -1,7 +1,7 @@
 package util;
 
 public class TestUtil {
-	public static long PAGE_LOAD_TIMEOUT = 20 ;
-	public static long IMPLICIT_WAIT = 20 ;
+	public static long PAGE_LOAD_TIMEOUT = 30 ;
+	public static long IMPLICIT_WAIT = 30 ;
 
 }
